07bd37dfb5fc459ad05a1e546afc67971cb057e1
prefered Username <zcw607@gmail.com>
2020-03-12 11:30:39 -0500

    [update_dataset_docs]

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -1,17 +1,9 @@
-from .builder import build_dataset
-from .cityscapes import CityscapesDataset
 from .coco import CocoDataset
-from .custom import CustomDataset
-from .dataset_wrappers import ConcatDataset, RepeatDataset
-from .loader import DistributedGroupSampler, GroupSampler, build_dataloader
 from .registry import DATASETS
-from .voc import VOCDataset
-from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
+from .voc import VOCDataset
+@DATASETS.register_module
+class MyDataset(XMLDataset):
 
-__all__ = [
-    'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset',
-    'CityscapesDataset', 'GroupSampler', 'DistributedGroupSampler',
-    'build_dataloader', 'ConcatDataset', 'RepeatDataset', 'WIDERFaceDataset',
-    'DATASETS', 'build_dataset'
-]
+    # CLASSES =('Dent', 'Scratch_or_spot')#, 'Shatter', 'Tear')
+    CLASSES = ('abyssinian', 'american_bulldog','american_pit_bull_terrier','basset','beagle','bengal','birman','bombay','boxer','british_shorthair','chihuahua','egyptian','english_cocker','english_setter','german_shorthaired','great_pyrenees','havanese','japanese_chin','keeshond','leonberger','maine_coon','miniature_pinscher','newfoundland','persian','pomeranian','pug','ragdoll','russian_blue','saint_bernard','samoyed','scottish_terrier','shiba_inu','siamese','sphynx','staffordshire','wheaten_terrier','yorkshire_terrier')
